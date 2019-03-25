83dad4045ce91d674d70d72f31e3dc6d26eab317
Forrest McDougal <forrestmcdougal@gmail.com>
2019-03-25 11:36:25 -0400

    add final fours

---
+++ final_four.py
@@ -0,0 +1,56 @@
+final_fours = {
+    2010: {
+        'Duke': True,
+        'Butler': False,
+        'Michigan State': False,
+        'West Virginia': False,
+    },
+    2011: {
+        'Connecticut': True,
+        'Butler': False,
+        'Kentucky': False,
+        'VCU': False
+    },
+    2012: {
+        'Kentucky': True,
+        'Kansas': False,
+        'Ohio State': False,
+        'Louisville': False
+    },
+    2013: {
+        'Lousiville': True,
+        'Michigan': False,
+        'Syracuse': False,
+        'Wichita State': False
+    },
+    2014: {
+        'Connecticut': True,
+        'Kentucky': False,
+        'Florida': False,
+        'Wisconsin': False
+    },
+    2015: {
+        'Duke': True,
+        'Wisconsin': False,
+        'Michigan State': False,
+        'Kentucky': False
+    },
+    2016: {
+        'Villanova': True,
+        'North Carolina': False,
+        'Oklahoma': False,
+        'Syracuse': False
+    },
+    2017: {
+        'North Carolina': True,
+        'Gonzaga': False,
+        'Oregon': False,
+        'South Carolina': False
+    },
+    2018: {
+        'Villanova': True,
+        'Michigan': False,
+        'Kansas': False,
+        'Loyola-Chicago': False
+    }
+}
