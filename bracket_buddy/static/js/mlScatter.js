22a831ad3d368867709a14c4719d89c5c8d76946
Forrest McDougal <forrestmcdougal@gmail.com>
2019-05-06 15:31:25 -0400

    tooltip and colors for scatter

--- bracket_buddy/static/js/mlScatter.js
+++ bracket_buddy/static/js/mlScatter.js
@@ -1,120 +1,131 @@
 let mlScatter;
 
 function makeMLScatterInit(ctx) {
-    let team1 = homeTeamDropdown.value;
-    let year1 = homeYearDropdown.value;
-    let team2 = awayTeamDropdown.value;
-    let year2 = awayYearDropdown.value;
+	let team1 = homeTeamDropdown.value;
+	let year1 = homeYearDropdown.value;
+	let team2 = awayTeamDropdown.value;
+	let year2 = awayYearDropdown.value;
 	d3.json(`/api/predictions/${team1}/${year1}/${team2}/${year2}`).then((predictData) => {
 		let homePoints = predictData['home_points'].map((d) => +d);
-        let awayPoints = predictData['away_points'].map((d) => +d);
-        let minHome = Math.min(...homePoints);
-        let maxHome = Math.max(...homePoints);
-        let minAway = Math.min(...awayPoints);
-        let maxAway = Math.max(...awayPoints);
-        let overallMin = Math.min(minHome, minAway) - 5;
-        let overallMax = Math.max(maxHome, maxAway) + 5;
-        console.log(overallMin);
-        console.log(overallMax);
-        let dataPoints = []
-        for (let i = 0; i < homePoints.length; i++) {
-            dataPoints.push({x: homePoints[i], y: awayPoints[i]});
-        }
-        let options = {
-				tooltips: {
-					label: function(tooltipItem) {
-						return `${team1}: ${Number(tooltipItem.xLabel)}, ${team2}: ${Number(tooltipItem.yLabel)}`
+		let awayPoints = predictData['away_points'].map((d) => +d);
+		let colors = predictData['scatter_color'].map((d) => d);
+		let minHome = Math.min(...homePoints);
+		let maxHome = Math.max(...homePoints);
+		let minAway = Math.min(...awayPoints);
+		let maxAway = Math.max(...awayPoints);
+		let overallMin = Math.min(minHome, minAway) - 1;
+		let overallMax = Math.max(maxHome, maxAway) + 1;
+		let dataPoints = [];
+		for (let i = 0; i < homePoints.length; i++) {
+			dataPoints.push({ x: homePoints[i], y: awayPoints[i] });
+		}
+		let options = {
+			tooltips: {
+				callbacks: {
+					label: function(tooltipItem, data) {
+						return `${team1}: ${Number(tooltipItem.xLabel).toFixed()}, ${team2}: ${Number(
+							tooltipItem.yLabel
+						).toFixed()}`;
 					}
-                },
-                scales: {
-                    xAxes: [{
-                        scaleLabel: {
-                            displayString: true,
-                            labelString: `${team1} Points`
-                        },
-                        ticks: {
-                            suggestedMin: overallMin,
-                            suggestedMax: overallMax
-                        }
-                    }],
-                    yAxes: [{
-                        scaleLabel: {
-                            displayString: true,
-                            labelString: `${team2} Points`
-                        },
-                        ticks: {
-                            suggestedMin: overallMin,
-                            suggestedMax: overallMax
-                        }
-                    }]
-
-                }
-        };
+				}
+			},
+			scales: {
+				xAxes: [
+					{
+						scaleLabel: {
+							displayString: true,
+							labelString: `${team1} Points`
+						},
+						ticks: {
+							suggestedMin: overallMin,
+							suggestedMax: overallMax
+						}
+					}
+				],
+				yAxes: [
+					{
+						scaleLabel: {
+							displayString: true,
+							labelString: `${team2} Points`
+						},
+						ticks: {
+							suggestedMin: overallMin,
+							suggestedMax: overallMax
+						}
+					}
+				]
+			}
+		};
 
 		mlScatter = new Chart(ctx, {
 			type: 'scatter',
 			data: {
-				datasets: [{
-                    data: dataPoints
-				}]
-            },
-            options: options
+				datasets: [
+					{
+						data: dataPoints,
+						pointBackgroundColor: colors
+					}
+				]
+			},
+			options: options
 		});
-
-
-    });
+	});
 }
 
 function makeMLScatter(data) {
-    let homePoints = predictData['home_points'].map((d) => +d);
-    let awayPoints = predictData['away_points'].map((d) => +d);
-    let minHome = Math.min(...homePoints);
-    let maxHome = Math.max(...homePoints);
-    let minAway = Math.min(...awayPoints);
-    let maxAway = Math.max(...awayPoints);
-    let overallMin = Math.min([minHome, minAway]) - 5;
-    let overallMax = Math.max([maxHome, maxAway]) + 5;
-    let dataPoints = []
-    for (let i = 0; i < homePoints.length; i++) {
-        dataPoints.push({x: homePoints[i], y: awayPoints[i]});
-    }
-    let options = {
-        tooltips: {
-            label: function(tooltipItem) {
-                return `${team1}: ${Number(tooltipItem.xLabel)}, ${team2}: ${Number(tooltipItem.yLabel)}`
-            }
-        },
-        scales: {
-            xAxes: {
-                scaleLabel: {
-                    displayString: true,
-                    labelString: `${team1} Points`
-                },
-                ticks: {
-                    min: overallMin,
-                    max: overallMax
-                }
-            },
-            yAxes: {
-                scaleLabel: {
-                    displayString: true,
-                    labelString: `${teams2} Points`
-                },
-                ticks: {
-                    min: overallMin,
-                    max: overallMax
-                }
-            }
-    }
-    }   
-    mlScatter.data = {
-            datasets: [{
-                data: [{
-                    x: homePoints,
-                    y: awayPoints,
-                }]
-            }]
-        };
-    mlScatter.options = options;
-    mlScatter.update()
+	let homePoints = predictData['home_points'].map((d) => +d);
+	let awayPoints = predictData['away_points'].map((d) => +d);
+	let minHome = Math.min(...homePoints);
+	let maxHome = Math.max(...homePoints);
+	let minAway = Math.min(...awayPoints);
+	let maxAway = Math.max(...awayPoints);
+	let overallMin = Math.min([ minHome, minAway ]) - 5;
+	let overallMax = Math.max([ maxHome, maxAway ]) + 5;
+	let dataPoints = [];
+	for (let i = 0; i < homePoints.length; i++) {
+		dataPoints.push({ x: homePoints[i], y: awayPoints[i] });
+	}
+	let options = {
+		tooltips: {
+			label: function(tooltipItem) {
+				return `${team1}: ${Number(tooltipItem.xLabel)}, ${team2}: ${Number(tooltipItem.yLabel)}`;
+			}
+		},
+		scales: {
+			xAxes: {
+				scaleLabel: {
+					displayString: true,
+					labelString: `${team1} Points`
+				},
+				ticks: {
+					min: overallMin,
+					max: overallMax
+				}
+			},
+			yAxes: {
+				scaleLabel: {
+					displayString: true,
+					labelString: `${teams2} Points`
+				},
+				ticks: {
+					min: overallMin,
+					max: overallMax
+				}
+			}
+		}
+	};
+	mlScatter.data = {
+		datasets: [
+			{
+				data: [
+					{
+						x: homePoints,
+						y: awayPoints
+					}
+				]
+			}
+		]
+	};
+	mlScatter.options = options;
+	mlScatter.update();
 }
